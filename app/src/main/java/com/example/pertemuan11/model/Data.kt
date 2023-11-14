bbc2f6ea893289cb82aaa6fa2fced7262f17dd63
Thiraf <anandahirzithirafi@mail.ugm.ac.id>
2023-11-14 08:13:37 +0700

    Revert "Minggu_2"
    This reverts commit edd0f533401dfccb0a257c8331fce7107d18f5c1.

--- app/src/main/java/com/example/pertemuan11/model/Data.kt
+++
@@ -1,9 +0,0 @@
-package com.example.pertemuan11.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Data (
-    @SerializedName("employee_name")
-    var employeeName:String,
-
-)
--- app/src/main/java/com/example/pertemuan11/model/Users.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pertemuan11.model
-
-import com.google.gson.annotations.SerializedName
-
-data class Users(
-    @SerializedName("data")
-    var data:List<Data>
-)
