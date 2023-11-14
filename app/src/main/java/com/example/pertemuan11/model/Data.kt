edd0f533401dfccb0a257c8331fce7107d18f5c1
Thiraf <anandahirzithirafi@mail.ugm.ac.id>
2023-11-14 08:12:55 +0700

    Minggu_2

---
+++ app/src/main/java/com/example/pertemuan11/model/Data.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan11.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Data (
+    @SerializedName("employee_name")
+    var employeeName:String,
+
+)
